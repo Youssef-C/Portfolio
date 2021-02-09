0391525f273d26352fefd53bcf3c445b8a8e684f
Youssef-C <youssef.chardi@student.kyh.se>
2021-02-09 14:55:28 +0100

    Second commit, projects tab almost finished

--- src/WorkTab.js
+++ src/WorkTab.js
@@ -2,6 +2,8 @@
 import styled from 'styled-components';
 import './App.css';
 import Quire from './Images/Quire.png';
+import ProgressBlog from './Images/Progress-Blog.png';
+import ShoeCard from './Images/3D-Card.png';
 
     //STYLED COMPONENTS
 //The container for the ProjectsList tab
@@ -18,25 +20,27 @@ function WorkTab () {
                         <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">QUIRE</a>
                     </div>
                     <a href="https://github.com/maxjander/fe20tp1_keyboard"> <img className="project-image" src={Quire} alt=""/></a>
-                    
                 </div>
                 
                 <div className="project-container">
                     <div className="overlay">
-                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">QUIRE</a>
+                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">Progress Blog!</a>
                     </div>
+                    <a href="https://github.com/maxjander/fe20tp1_keyboard"> <img className="project-image" src={ProgressBlog} alt=""/></a>
                 </div>
 
                 <div className="project-container">
                     <div className="overlay">
-                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">QUIRE</a>
+                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">3D Card!</a>
                     </div>
+                    <a href="https://github.com/maxjander/fe20tp1_keyboard"> <img className="project-image" src={ShoeCard} alt=""/></a>
                 </div>
 
                 <div className="project-container">
                     <div className="overlay">
-                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">QUIRE</a>
+                        <a href="https://github.com/maxjander/fe20tp1_keyboard" className="project-title">This Portfolio</a>
                     </div>
+                    <a href="https://github.com/maxjander/fe20tp1_keyboard"> <img className="project-image" src={Quire} alt=""/></a>
                 </div>
             </div>
         </div>
